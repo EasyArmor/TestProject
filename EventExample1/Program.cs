676dd4659bc05e42953b3d0d7f95d09a5538854d
BlackArmor <Vadim_Belikov@mail.ru>
2014-08-08 14:42:38 +0300

    Add work files of project

---
+++ EventExample1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventExample1
+{
+    delegate void Cheeze();
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Nose nose = new Nose();
+            nose.ItchingINTheNose += new Cheeze(nose.MethodCheeze);            
+            nose.StartEvent();
+        }
+
+                
+    }
+
+    class Nose
+    {
+        public event Cheeze ItchingINTheNose;
+
+        public void StartEvent()
+        {
+            if (ItchingINTheNose != null)
+                ItchingINTheNose();
+        }
+
+        public void MethodCheeze()
+        {
+            Console.WriteLine("Чьху!!!");
+        }
+    }
+}
