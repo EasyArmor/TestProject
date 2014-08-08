46e32947b4a71e7aa9f9596bb5ce105f3ef33254
BlackArmor <Vadim_Belikov@mail.ru>
2014-08-08 16:16:16 +0300

    Add

--- EventExample1/Class3.cs
+++ EventExample1/Class3.cs
@@ -11,7 +11,7 @@ namespace EventExample1
         private Del del;
 
         public void RegistrationMethod(Del d)
-        {           
+        {
             del += d;
         }
 
@@ -25,3 +25,4 @@ namespace EventExample1
         }
     }
 }
+
